091444421a250ed02b4fe34bec04fe31bb49e3e1
Connor <csully710@gmail.com>
2025-09-18 11:01:19 -0400

    uibuild

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Note = require('./models/note')
 
 let notes = [
   {
@@ -17,7 +19,7 @@ let notes = [
     content: "GET and POST are the most important methods of HTTP protocol",
     important: true
   }
-]
+];
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method);
@@ -47,27 +49,22 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes);
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    const note = notes.find(note => note.id === id);
-
-    if(note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note);
-    }
-    else {
-        response.statusMessage = `Note ${id} does not exist`;
-        response.status(404).end();
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
     const id = request.params.id;
     notes = notes.filter(note => note.id !== id);
     
-    response.status(204).end()
+    response.status(204).end();
 })
 
 app.post('/api/notes', (request, response) => {
@@ -79,20 +76,21 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         id: generateId()
-    }
-    notes = notes.concat(note)
+    })
     
-    response.json(note);
+    note.save().then(savedNote => {
+        response.json(savedNote);
+    })
 })
 
 app.use(unknownEndpoint)
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
